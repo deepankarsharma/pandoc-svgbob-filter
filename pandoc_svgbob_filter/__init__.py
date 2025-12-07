48ab75bf0f5ffa2a3ff24bb94317c87251f31018
Deepankar Sharma <deepankarsharma@gmail.com>
2025-12-07 18:05:03 -0500

    add support for svgbob code blocks and svgbob_cli binary
    - Handle CodeBlock elements with svgbob class, not just Links
    - Support both svgbob_cli (newer) and svgbob (older) binary names
    - Extract get_options() helper to reduce code duplication

--- pandoc_svgbob_filter/__init__.py
+++ pandoc_svgbob_filter/__init__.py
@@ -10,7 +10,7 @@ from shutil import which
 
 class SvgbobInline(object):
     """
-    Converts alone Link in svgbob class to Image
+    Converts CodeBlock or Link with svgbob class to Image
     requires `svgbob` in PATH
     option can be provided as attributes or can set default values in yaml metadata block
 
@@ -25,29 +25,67 @@ class SvgbobInline(object):
 
     def __init__(self):
         self.dir_to = "svg"
-        assert which("svgbob"), "svgbob is not in path"
+        # Support both svgbob_cli (newer) and svgbob (older) binary names
+        if which("svgbob_cli"):
+            self.svgbob_cmd = "svgbob_cli"
+        elif which("svgbob"):
+            self.svgbob_cmd = "svgbob"
+        else:
+            raise AssertionError("svgbob or svgbob_cli is not in path")
+
+    def get_options(self, attributes, doc):
+        """Extract svgbob options from attributes and metadata."""
+        meta_font_family = doc.get_metadata("svgbob.font-family", "Arial")
+        meta_font_size = doc.get_metadata("svgbob.font-size", 14)
+        meta_scale = doc.get_metadata("svgbob.scale", 1)
+        meta_stroke_width = doc.get_metadata("svgbob.stroke-width", 2)
+
+        font_family = attributes.get("font-family", meta_font_family)
+        font_size = attributes.get("font-size", meta_font_size)
+        scale = attributes.get("scale", meta_scale)
+        stroke_width = attributes.get("stroke-width", meta_stroke_width)
+
+        svgbob_option = " ".join([
+            '--font-family "{}"'.format(font_family) if font_family is not None else "",
+            "--font-size {}".format(font_size) if font_size is not None else "",
+            "--scale {}".format(scale) if scale is not None else "",
+            "--stroke-width {}".format(stroke_width) if stroke_width is not None else "",
+        ])
+        return svgbob_option
+
+    def process_codeblock(self, elem, doc):
+        """Process a CodeBlock with svgbob class."""
+        if not os.path.exists(self.dir_to):
+            os.mkdir(self.dir_to)
+
+        data = elem.text
+        counter = hashlib.sha1(data.encode("utf-8")).hexdigest()[:8]
+        self.basename = "/".join([self.dir_to, str(counter)])
+
+        _format = "svg"
+        linkto = os.path.abspath(".".join([self.basename, _format])).replace("\\", "/")
+
+        svgbob_option = self.get_options(elem.attributes, doc)
+
+        command = "{} {} -o {}".format(self.svgbob_cmd, svgbob_option, linkto)
+        pf.debug(command)
+
+        proc = sp.Popen(command, shell=True, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
+        proc.communicate(input=data.encode("utf-8"))
+
+        pf.debug("[codeblock] generate svgbob to {}".format(linkto))
+
+        elem.classes.remove("svgbob")
+        return pf.Para(pf.Image(url=linkto, classes=elem.classes,
+                                identifier=elem.identifier, attributes=elem.attributes))
 
     def action(self, elem, doc):
-        if isinstance(elem, pf.Link) and "svgbob" in elem.classes:
+        if isinstance(elem, pf.CodeBlock) and "svgbob" in elem.classes:
+            return self.process_codeblock(elem, doc)
+        elif isinstance(elem, pf.Link) and "svgbob" in elem.classes:
             fn = elem.url
-            options = elem.attributes
             caption = elem.content
 
-            meta_font_family = doc.get_metadata("svgbob.font-family", "Arial")
-            meta_font_size = doc.get_metadata("svgbob.font-size", 14)
-            meta_scale = doc.get_metadata("svgbob.scale", 1)
-            meta_stroke_width = doc.get_metadata("svgbob.stroke-width", 2)
-
-            font_family = options.get("font-family", meta_font_family)
-            font_size = options.get("font-size", meta_font_size)
-            scale = options.get("scale", meta_scale)
-            stroke_width = options.get("stroke-width", meta_stroke_width)
-            svgbob_option = " ".join([
-                '--font-family "{}"'.format(font_family) if font_family is not None else "",
-                "--font-size {}".format(font_size) if font_size is not None else "",
-                "--scale {}".format(scale) if scale is not None else "",
-                "--stroke-width {}".format(stroke_width) if stroke_width is not None else "",
-            ])
             if not os.path.exists(self.dir_to):
                 os.mkdir(self.dir_to)
 
@@ -56,28 +94,18 @@ class SvgbobInline(object):
             self.basename = "/".join([self.dir_to, str(counter)])
 
             _format = "svg"
-            # if doc.format in ["latex"]:
-            #     format = "pdf"
-            # elif doc.format in ["html", "html5"]:
-            #     format = "svg"
-            # else:
-            #     format = "png"
-
             fn = os.path.abspath(fn)
             linkto = os.path.abspath(".".join([self.basename, _format])).replace("\\", "/")
 
-            command = "svgbob {} {} -o {}".format(fn, svgbob_option, linkto)
+            svgbob_option = self.get_options(elem.attributes, doc)
+            command = "{} {} {} -o {}".format(self.svgbob_cmd, fn, svgbob_option, linkto)
             pf.debug(command)
             sp.Popen(command, shell=True, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
 
             pf.debug("[inline] generate svgbob from {} to {}".format(fn, linkto))
             elem.classes.remove("svgbob")
-            elem = pf.Image(*caption, classes=elem.classes, url=linkto,
+            return pf.Image(*caption, classes=elem.classes, url=linkto,
                             identifier=elem.identifier, title="fig:", attributes=elem.attributes)
-
-            # pf.debug(elem)
-
-            return elem
 
 
 def main(doc=None):
